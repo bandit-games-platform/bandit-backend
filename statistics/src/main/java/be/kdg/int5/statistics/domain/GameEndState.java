e7eac481329b4ceca0e2609448eefe21ea97d6bd
Inês da Costa Bastos <ines.dacostabastos@student.kdg.be>
2024-11-15 00:37:58 +0100

    domain for statistics context done

---
+++ statistics/src/main/java/be/kdg/int5/statistics/domain/GameEndState.java
@@ -0,0 +1,7 @@
+package be.kdg.int5.statistics.domain;
+
+public enum GameEndState {
+    WIN,
+    LOSS,
+    DRAW
+}
---
+++ statistics/src/main/java/be/kdg/int5/statistics/domain/PlayerId.java
@@ -0,0 +1,6 @@
+package be.kdg.int5.statistics.domain;
+
+import java.util.UUID;
+
+public record PlayerId(UUID uuid) {
+}
