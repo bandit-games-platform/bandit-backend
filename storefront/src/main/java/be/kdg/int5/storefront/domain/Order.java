f2b7de26c4f5adb62aa2c3673a794eb1a1393792
DeanTerneu <dean.terneu@student.kdg.be>
2024-11-16 19:23:12 +0100

    fix domain entities

--- storefront/src/main/java/be/kdg/int5/storefront/domain/Order.java
+++ storefront/src/main/java/be/kdg/int5/storefront/domain/Order.java
@@ -5,10 +5,10 @@ import java.util.Objects;
 import java.util.UUID;
 
 public class Order {
-    private OrderId id;
-    private ProductId productId;
-    private CustomerId customerId;
-    private LocalDateTime orderDate;
+    private final OrderId id;
+    private final ProductId productId;
+    private final CustomerId customerId;
+    private final LocalDateTime orderDate;
     private LocalDateTime  orderCompletedAt;
     private OrderStatus orderStatus;
 
@@ -19,7 +19,6 @@ public class Order {
         Objects.requireNonNull(customerId);
         Objects.requireNonNull(orderDate);
 
-        this.id = id;
         this.productId = productId;
         this.customerId = customerId;
         this.orderDate = orderDate;
