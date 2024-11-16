d42a490fdafedd46443e93a7d82965a0109bc2f4
DeanTerneu <dean.terneu@student.kdg.be>
2024-11-16 19:13:17 +0100

    merging with main

--- gameRegistry/src/main/java/be/kdg/int5/gameRegistry/adapters/in/GamesOverviewRestController.java
+++ gameRegistry/src/main/java/be/kdg/int5/gameRegistry/adapters/in/GamesOverviewRestController.java
@@ -1,16 +1,46 @@
 package be.kdg.int5.gameRegistry.adapters.in;
 
+import be.kdg.int5.gameRegistry.adapters.in.dto.LoadAchievementDto;
+import be.kdg.int5.gameRegistry.adapters.in.dto.LoadGameDto;
+import be.kdg.int5.gameRegistry.adapters.in.dto.LoadRuleDto;
+import be.kdg.int5.gameRegistry.domain.Game;
+import be.kdg.int5.gameRegistry.port.in.query.GameListQuery;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/games")
 public class GamesOverviewRestController {
 
-    @GetMapping("/overview")
-    public void getGameOverview(){
-        System.out.println("Hello Game Registry");
+    private final GameListQuery gameListQuery;
+
+    public GamesOverviewRestController(GameListQuery gameListQuery) {
+        this.gameListQuery = gameListQuery;
     }
 
+    @GetMapping("/overview")
+    public ResponseEntity<List<LoadGameDto>> getGameOverview() {
+
+        List<Game> games = gameListQuery.retrieveGamesWithIcon();
+
+        List<LoadGameDto> gameDtos = new ArrayList<>();
+
+        games.forEach(game -> {
+
+            LoadGameDto gameDto = new LoadGameDto(
+                    game.getTitle(),
+                    game.getDescription(),
+                    game.getIcon().url().url(),
+                    game.getId().uuid()
+            );
+            gameDtos.add(gameDto);
+        });
+
+        return ResponseEntity.ok(gameDtos);
+    }
 }
--- gameRegistry/src/main/java/be/kdg/int5/gameRegistry/core/GameListQueryImpl.java
+++ gameRegistry/src/main/java/be/kdg/int5/gameRegistry/core/GameListQueryImpl.java
@@ -3,6 +3,7 @@ package be.kdg.int5.gameRegistry.core;
 import be.kdg.int5.gameRegistry.domain.Game;
 import be.kdg.int5.gameRegistry.port.in.query.GameListQuery;
 import be.kdg.int5.gameRegistry.port.out.LoadGamesPort;
+import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,8 @@ public class GameListQueryImpl implements GameListQuery {
         this.loadGamesPort = loadGamesPort;
     }
 
+    //TODO remove transactional
+    @Transactional
     @Override
     public List<Game> retrieveGamesWithIcon() {
 
