75283ef3b58ea6ae1525a49df49c088fdbe2f2f8
DeanTerneu <dean.terneu@student.kdg.be>
2024-11-16 09:35:52 +0100

    fix/gameRegistry/add-not-null

--- storefront/src/main/java/be/kdg/int5/storefront/domain/Order.java
+++ storefront/src/main/java/be/kdg/int5/storefront/domain/Order.java
@@ -1,6 +1,7 @@
 package be.kdg.int5.storefront.domain;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Order {
     private OrderId id;
@@ -11,6 +12,11 @@ public class Order {
     private OrderStatus orderStatus;
 
     public Order(OrderId id, ProductId productId, CustomerId customerId, LocalDateTime orderDate) {
+        Objects.requireNonNull(id);
+        Objects.requireNonNull(productId);
+        Objects.requireNonNull(customerId);
+        Objects.requireNonNull(orderDate);
+
         this.id = id;
         this.productId = productId;
         this.customerId = customerId;
