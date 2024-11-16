0aaf4a24efaebb74ae089a71022c6c8c02b6d37a
Ayodeji ALLI-SMITH <ayodeji.allismith@student.kdg.be>
2024-11-16 12:55:48 +0100

    player library item set added

--- player/src/main/java/be/kdg/int5/player/domain/PlayerLibrary.java
+++ player/src/main/java/be/kdg/int5/player/domain/PlayerLibrary.java
@@ -1,9 +1,14 @@
 package be.kdg.int5.player.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PlayerLibrary {
     private final PlayerId playerId;
+    private Set<PlayerLibraryItem> playerLibraryItems;
 
     public PlayerLibrary(final PlayerId playerId) {
         this.playerId = playerId;
+        this.playerLibraryItems = new HashSet<>();
     }
 }
