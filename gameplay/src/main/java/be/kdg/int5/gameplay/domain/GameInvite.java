2c68430f06f187ffe2369ceb8b5e63d471874e9c
boldi-kdg <boldizsar.olajos@student.kdg.be>
2024-11-16 20:02:09 +0100

    Add validation to GameInvite

--- gameplay/src/main/java/be/kdg/int5/gameplay/domain/GameInvite.java
+++ gameplay/src/main/java/be/kdg/int5/gameplay/domain/GameInvite.java
@@ -1,11 +1,25 @@
 package be.kdg.int5.gameplay.domain;
 
+import java.util.Objects;
+
 public class GameInvite {
     private final PlayerId invited;
     private boolean accepted;
 
     public GameInvite(PlayerId invited) {
         this.accepted = false;
-        this.invited = invited;
+        this.invited = Objects.requireNonNull(invited);
+    }
+
+    public PlayerId getInvited() {
+        return invited;
+    }
+
+    public boolean isAccepted() {
+        return accepted;
+    }
+
+    public void setAccepted(boolean accepted) {
+        this.accepted = accepted;
     }
 }
