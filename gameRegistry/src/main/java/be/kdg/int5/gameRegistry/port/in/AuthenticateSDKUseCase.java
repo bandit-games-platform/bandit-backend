1f8c2aa2b2c55d7e574adb0df4297944dd67a13f
boldi-kdg <boldizsar.olajos@student.kdg.be>
2024-11-19 08:02:48 +0100

    Remove even more extra lines

--- gameRegistry/src/main/java/be/kdg/int5/gameRegistry/port/in/AuthenticateSDKUseCase.java
+++ gameRegistry/src/main/java/be/kdg/int5/gameRegistry/port/in/AuthenticateSDKUseCase.java
@@ -1,7 +1,6 @@
 package be.kdg.int5.gameRegistry.port.in;
 
 public interface AuthenticateSDKUseCase {
-
     String authenticate(AuthenticateSDKCommand command) throws InvalidApiKeyException;
 
     class InvalidApiKeyException extends Exception {}
--- gameRegistry/src/main/java/be/kdg/int5/gameRegistry/port/out/LoadDeveloperApiKeyPort.java
+++ gameRegistry/src/main/java/be/kdg/int5/gameRegistry/port/out/LoadDeveloperApiKeyPort.java
@@ -5,7 +5,5 @@ import be.kdg.int5.gameRegistry.domain.DeveloperApiKey;
 import java.util.Optional;
 
 public interface LoadDeveloperApiKeyPort {
-
     Optional<DeveloperApiKey> load(String apiKey);
-
 }
