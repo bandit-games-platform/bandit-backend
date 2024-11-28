b3cebef49c2c02d46f483ab4781d379ffb77391d
Ayodeji ALLI-SMITH <ayodeji.allismith@student.kdg.be>
2024-11-28 10:37:38 +0100

    sql changes

--- statistics/src/main/resources/data.sql
+++ statistics/src/main/resources/data.sql
@@ -6,9 +6,9 @@ VALUES
 
 INSERT INTO statistics.player_achievements_progress (achievement_progress_id, achievement_id, player_id, game_id, counter_total)
 VALUES
-    ('a91baf13-3965-45ff-9ee5-13be8f105f23', '123e4567-e89b-12d3-a456-426614174001', '8449ba7c-194c-4e51-b060-cd88cc498836', 'd77e1d1f-6b46-4c89-9290-3b9cf8a7c001', 2),
-    ('29242f25-1438-49a2-84ca-1cbd285fba30', '123e4567-e89b-12d3-a456-426614174002', '8449ba7c-194c-4e51-b060-cd88cc498836', 'd77e1d1f-6b46-4c89-9290-3b9cf8a7c001', 4),
-    ('c63704ae-f6bc-4a26-bfd3-3ab890bcd159', '223e4567-e89b-12d3-a456-426614174004', '8449ba7c-194c-4e51-b060-cd88cc498836', 'd77e1d1f-6b46-4c89-9290-3b9cf8a7c002', 25);
+    ('a91baf13-3965-45ff-9ee5-13be8f105f23', '4964f6f7-5cee-3619-8caf-754d10078579', '8449ba7c-194c-4e51-b060-cd88cc498836', 'd77e1d1f-6b46-4c89-9290-3b9cf8a7c001', 2),
+    ('29242f25-1438-49a2-84ca-1cbd285fba30', '28172f39-8abc-3499-819f-edc22a116468', '8449ba7c-194c-4e51-b060-cd88cc498836', 'd77e1d1f-6b46-4c89-9290-3b9cf8a7c001', 4),
+    ('c63704ae-f6bc-4a26-bfd3-3ab890bcd159', '3eaf18ca-2089-354f-81b4-6d0df2622207', '8449ba7c-194c-4e51-b060-cd88cc498836', 'd77e1d1f-6b46-4c89-9290-3b9cf8a7c002', 25);
 
 
 
@@ -28,7 +28,7 @@ INSERT INTO statistics.completed_game_sessions (
 )
 VALUES
 -- Session 1: Close match
-('e4529160-dea4-41f3-83f9-7794be39493b', '2024-11-03 11:00:00', '2024-11-03 11:35:00', 'DRAW', 30, 12.0, 150, 150, 70, 'Archer', false, '8449ba7c-194c-4e51-b060-cd88cc498836', 'd77e1d1f-6b46-4c89-9290-3b9cf8a7c002'),
+('e4529160-dea4-41f3-83f9-7794be39493b', '2024-11-03 11:00:00', '2024-11-03 11:35:00', 'DRAW', 30, 12.0, null, null, 70, 'Archer', false, '8449ba7c-194c-4e51-b060-cd88cc498836', 'd77e1d1f-6b46-4c89-9290-3b9cf8a7c002'),
 
 -- Session 2: Dominant win
 ('7f4afdfd-1ef8-4af4-af4f-f5da15c6c1f9', '2024-11-04 13:30:00', '2024-11-04 14:00:00', 'WIN', 15, 10.5, 200, 50, 40, 'Warrior', true, '8449ba7c-194c-4e51-b060-cd88cc498836', 'd77e1d1f-6b46-4c89-9290-3b9cf8a7c001'),
