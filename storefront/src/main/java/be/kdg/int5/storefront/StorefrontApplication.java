bb578fd5a9a24f8b16be8fc98381b998b8a4ad85
Roman Gordon <roman.gordon@student.kdg.be>
2024-12-05 10:29:31 +0100

    Removed unused imports from StorefrontApplication

--- storefront/src/main/java/be/kdg/int5/storefront/StorefrontApplication.java
+++ storefront/src/main/java/be/kdg/int5/storefront/StorefrontApplication.java
@@ -1,7 +1,5 @@
 package be.kdg.int5.storefront;
 
-import com.stripe.Stripe;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
