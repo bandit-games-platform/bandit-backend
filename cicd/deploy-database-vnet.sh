91705ae518ee936875d95e1ff629a13a024cba57
Roman Gordon <roman.gordon@student.kdg.be>
2024-12-10 10:32:06 +0100

    Attempting to use init script instead of inline commands

--- cicd/deploy-database-vnet.sh
+++ cicd/deploy-database-vnet.sh
@@ -95,8 +95,9 @@ if [ -z "$DB_EXISTS" ]; then
         --resource-group $RG_NAME \
         --environment $ENV_NAME \
         --image postgres:latest \
+        --env-vars PG_ADMIN_PASSWORD="$PG_ADMIN_PASSWORD" DB_SERVER_NAME=$DB_SERVER_NAME PG_ADMIN_USER="$PG_ADMIN_USER" PG_NON_ADMIN_USER="$PG_NON_ADMIN_USER" PG_NON_ADMIN_PASSWORD="$PG_NON_ADMIN_PASSWORD" \
         --command "/bin/sh" \
-        --args "-c", "PGPASSWORD=$PG_ADMIN_PASSWORD psql -h $DB_SERVER_NAME.postgres.database.azure.com -U $PG_ADMIN_USER -d bandit_db -c \"CREATE SCHEMA IF NOT EXISTS chatbot; CREATE SCHEMA IF NOT EXISTS gameplay; CREATE SCHEMA IF NOT EXISTS game_registry; CREATE SCHEMA IF NOT EXISTS player; CREATE SCHEMA IF NOT EXISTS statistics; CREATE SCHEMA IF NOT EXISTS storefront;\" && PGPASSWORD=$PG_ADMIN_PASSWORD psql -h $DB_SERVER_NAME.postgres.database.azure.com -U $PG_ADMIN_USER -d bandit_db -c \"CREATE USER $PG_NON_ADMIN_USER WITH PASSWORD '$PG_NON_ADMIN_PASSWORD';\" && PGPASSWORD=$PG_ADMIN_PASSWORD psql -h $DB_SERVER_NAME.postgres.database.azure.com -U $PG_ADMIN_USER -d bandit_db -c \"SELECT * FROM information_schema.schemata;\""
+        --args "-c", "curl -o /tmp/init-db.sh ${DB_INIT_URL} && chmod +x /tmp/init-db.sh && /tmp/init-db.sh"
 
 #    az containerapp delete --name init-container --resource-group $RG_NAME --yes
 
