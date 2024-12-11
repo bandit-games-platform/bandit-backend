99606fb59cd3a2e37afa1145941cb1ad50804e03
Roman Gordon <roman.gordon@student.kdg.be>
2024-12-11 23:50:04 +0100

    Attempting to add extra configuration to build.gradle file

--- cicd/deploy-database-vnet.sh
+++ cicd/deploy-database-vnet.sh
@@ -98,6 +98,15 @@ if [ -z "$DB_EXISTS" ]; then
         --command "/bin/sh" \
         --args "-c 'while true; do echo hello; sleep 10; done'"
 
+        # --command "/bin/sh -c 'PGPASSWORD=$PG_ADMIN_PASSWORD psql -h $DB_SERVER_NAME.postgres.database.azure.com -U $PG_ADMIN_USER -d bandit_db -c \"
+        #                    CREATE SCHEMA IF NOT EXISTS chatbot;
+        #                    CREATE SCHEMA IF NOT EXISTS gameplay;
+        #                    CREATE SCHEMA IF NOT EXISTS game_registry;
+        #                    CREATE SCHEMA IF NOT EXISTS player;
+        #                    CREATE SCHEMA IF NOT EXISTS statistics;
+        #                    CREATE SCHEMA IF NOT EXISTS storefront;
+        #                    \" && PGPASSWORD=$PG_ADMIN_PASSWORD psql -h $DB_SERVER_NAME.postgres.database.azure.com -U $PG_ADMIN_USER -d bandit_db -c \"CREATE USER $PG_NON_ADMIN_USER WITH PASSWORD '$PG_NON_ADMIN_PASSWORD';\" && PGPASSWORD=$PG_ADMIN_PASSWORD psql -h $DB_SERVER_NAME.postgres.database.azure.com -U $PG_ADMIN_USER -d bandit_db -c \"SELECT * FROM information_schema.schemata;\"'" "docker-entrypoint.sh"
+
 #    az containerapp delete --name init-container --resource-group $RG_NAME --yes
 
     echo "PostgreSQL server $DB_SERVER_NAME created."
