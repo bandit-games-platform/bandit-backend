74f2cfb628037c0fae7e7ef2b6d1277031cbf099
Inês da Costa Bastos <ines.dacostabastos@student.kdg.be>
2024-12-14 00:37:28 +0100

    setting environment variables in update command

--- cicd/deploy-rabbitmq-prod.sh
+++ cicd/deploy-rabbitmq-prod.sh
@@ -78,10 +78,12 @@ if [ -z "$RABBITMQ_EXISTS" ]; then
   az containerapp create \
     --name $CONTAINER_NAME \
     --resource-group $RESOURCE_GROUP \
-    --env-vars RABBITMQ_DEFAULT_USER="$RABBITMQ_USER" RABBITMQ_DEFAULT_PASS="$RABBITMQ_PASSWORD" \
     --yaml ./cicd/rabbitmq-containerapp.yml
 
-    echo "Rabbit user: $RABBITMQ_DEFAULT_USER, $RABBITMQ_USER"
+  az containerapp update \
+    --name $CONTAINER_NAME \
+    --resource-group $RESOURCE_GROUP \
+    --set-env-vars RABBITMQ_DEFAULT_USER="$RABBITMQ_USER" RABBITMQ_DEFAULT_PASS="$RABBITMQ_PASSWORD"
 
   echo "Creating $CONTAINER_NAME containerapp."
 else
