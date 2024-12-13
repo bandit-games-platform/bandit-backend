87408ed2d932ccda508acd2038f4fb2ad3335150
Inês da Costa Bastos <ines.dacostabastos@student.kdg.be>
2024-12-13 19:31:14 +0100

    added creating env, vnet and subnets for prod

--- cicd/deploy-rabbitmq-prod.sh
+++ cicd/deploy-rabbitmq-prod.sh
@@ -8,15 +8,86 @@
 #------------------------------------------------------------------------------------------------
 
 
+VNET_NAME="banditProdVnet"
+SUBNET_NAME="prodSubnet"
+SUBNET_RABBITMQ_NAME="prodRabbitSubnet"
+
 CONTAINER_NAME="rabbitmq-prod-container"
 RESOURCE_GROUP="rg_bandit_games_prod"
+ENV_NAME="env-prod-containers"
 IMAGE="$REGISTRY_USERNAME".azurecr.io/rabbitmq:3.13.7-management-alpine
 
 
+# Vnet and Subnets
+VNET_EXISTS=$(az network vnet list --resource-group $RESOURCE_GROUP --query "[?name=='$VNET_NAME'].name" -o tsv)
+
+if [ -z "$VNET_EXISTS" ]; then
+    # vnet
+    az network vnet create \
+      --name $VNET_NAME \
+      --resource-group $RESOURCE_GROUP \
+      --location northeurope \
+      --address-prefix 10.0.0.0/16
+
+    # contexts subnet
+    az network vnet subnet create \
+      --name $SUBNET_NAME \
+      --resource-group $RESOURCE_GROUP \
+      --vnet-name $VNET_NAME \
+      --address-prefix 10.0.1.0/24
+
+    az network vnet subnet update \
+      --resource-group $RESOURCE_GROUP \
+      --vnet-name $VNET_NAME \
+      --name $SUBNET_NAME \
+      --delegations Microsoft.App/environments
+
+    # rabbitmq subnet
+    az network vnet subnet create \
+      --name $SUBNET_SUBNET_RABBITMQ_NAME \
+      --resource-group $RESOURCE_GROUP \
+      --vnet-name $VNET_NAME \
+      --address-prefix 10.0.2.0/24
+
+    az network vnet subnet update \
+      --resource-group $RESOURCE_GROUP \
+      --vnet-name $VNET_NAME \
+      --name $SUBNET_SUBNET_RABBITMQ_NAME \
+      --delegations Microsoft.DBforPostgreSQL/flexibleServers
+
+    echo "VNet $VNET_NAME created."
+    echo "Subnet $SUBNET_NAME created."
+    echo "Subnet $SUBNET_RABBITMQ_NAME created."
+else
+    echo "VNet $VNET_NAME already exists."
+fi
+
+
+# Containerapp Environment
+ENV_EXISTS=$(az containerapp env list --resource-group $RESOURCE_GROUP --query "[?name=='$ENV_NAME'].name" -o tsv)
+
+if [ -z "$ENV_EXISTS" ]; then
+    INFRASTRUCTURE_SUBNET=$(az network vnet subnet show --resource-group ${RESOURCE_GROUP} --vnet-name $VNET_NAME --name ${SUBNET_NAME} --query "id" -o tsv | tr -d '[:space:]')
+
+    az containerapp env create \
+      --name $ENV_NAME \
+      --resource-group $RESOURCE_GROUP \
+      --location northeurope \
+      --infrastructure-subnet-resource-id "$INFRASTRUCTURE_SUBNET"
+
+    echo "Container Apps environment $ENV_NAME created."
+else
+    echo "Container Apps environment $ENV_NAME already exists."
+fi
+
+
+# RabbitMQ Containerapp
 echo "Deploying RabbitMQ containerapp..."
+
 az containerapp create \
   --name $CONTAINER_NAME \
   --resource-group $RESOURCE_GROUP \
+  --environment $ENV_NAME \
   --image $IMAGE \
   --target-port 5672 \
   --ingress external \
