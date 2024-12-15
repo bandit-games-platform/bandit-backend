e64636bd508297097df893c70b8106cad475d8d1
Inês da Costa Bastos <ines.dacostabastos@student.kdg.be>
2024-12-15 01:29:59 +0100

    starting docker daemon second attempt

--- cicd/deploy-rabbitmq-prod.sh
+++ cicd/deploy-rabbitmq-prod.sh
@@ -59,16 +59,6 @@ fi
 
 
 # RabbitMQ Containerapp
-
-#echo "Checking Docker daemon..."
-#if ! sudo systemctl is-active --quiet docker; then
-#    echo "Docker daemon is not running. Starting Docker..."
-#    sudo systemctl start docker
-#    sudo systemctl enable docker
-#else
-#    echo "Docker daemon is already running."
-#fi
-
 echo "Deploying RabbitMQ containerapp..."
 RABBITMQ_EXISTS=$(az containerapp list --resource-group $RESOURCE_GROUP --query "[?name=='$CONTAINER_NAME'].name" -o tsv)
 
