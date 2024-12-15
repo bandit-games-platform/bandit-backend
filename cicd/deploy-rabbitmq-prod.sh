54eed8d693d0ff9cc1ec250c0ad462eb84a6cfda
Inês da Costa Bastos <ines.dacostabastos@student.kdg.be>
2024-12-15 01:44:03 +0100

    added environment

--- cicd/deploy-rabbitmq-prod.sh
+++ cicd/deploy-rabbitmq-prod.sh
@@ -76,6 +76,7 @@ if [ -z "$RABBITMQ_EXISTS" ]; then
   az containerapp create \
     --name $CONTAINER_NAME \
     --resource-group $RESOURCE_GROUP \
+    --environment $ENV_NAME \
     --image acrbanditgamesdev.azurecr.io/rabbitmq:3.13.7-management-alpine \
     --target-port 15672 \
     --ingress external \
