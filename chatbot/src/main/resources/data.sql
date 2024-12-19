aff83cfb29cf5a239b02f8aa720fb1712485a34e
Roman Gordon <roman.gordon@student.kdg.be>
2024-12-19 16:10:20 +0000

    CICD now successfully deploys backend, keycloak and rabbit

--- chatbot/src/main/resources/data.sql
+++ chatbot/src/main/resources/data.sql
@@ -3,7 +3,8 @@
 INSERT INTO chatbot.game_details (game_id, title, description) VALUES
 ('d77e1d1f-6b46-4c89-9290-3b9cf8a7c001', 'Battleship', 'A strategic naval combat game'),
 ('d77e1d1f-6b46-4c89-9290-3b9cf8a7c002', 'Chess', 'A classic strategy game of kings and queens'),
-('d77e1d1f-6b46-4c89-9290-3b9cf8a7c003', 'Snakes and Ladders', 'A game of ups and downs');
+('d77e1d1f-6b46-4c89-9290-3b9cf8a7c003', 'Snakes and Ladders', 'A game of ups and downs'),
+('b6572f1b-cd36-3ebb-b292-6cfd0b92f8e9', 'Button Game', 'A thrilling strategy game consisting of two players and not a normal button... but a magic button... (drumrolls)');
 
 -- Seeding data for `rule` table
 INSERT INTO chatbot.game_rule (uuid, step_number, rule, game_details) VALUES
@@ -17,4 +18,7 @@ INSERT INTO chatbot.game_rule (uuid, step_number, rule, game_details) VALUES
 
 -- Rules for Snakes and Ladders
 ('c32e4567-e89b-12d3-a456-426614174014', 1, 'Roll the dice to move your token','d77e1d1f-6b46-4c89-9290-3b9cf8a7c003'),
-('c32e4567-e89b-12d3-a456-426614174015', 2, 'Climb ladders and slide down snakes based on the grid position','d77e1d1f-6b46-4c89-9290-3b9cf8a7c003');
+('c32e4567-e89b-12d3-a456-426614174015', 2, 'Climb ladders and slide down snakes based on the grid position','d77e1d1f-6b46-4c89-9290-3b9cf8a7c003'),
+
+('c32e4567-e89b-12d3-a456-426614174016', 1, 'Click button between 1 and 3 times', 'b6572f1b-cd36-3ebb-b292-6cfd0b92f8e9'),
+('c32e4567-e89b-12d3-a456-426614174017', 2, 'Make your friend guess how many times you pressed the button, dont forget to submit', 'b6572f1b-cd36-3ebb-b292-6cfd0b92f8e9');
--- gameplay/src/main/resources/data.sql
+++ gameplay/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 
 INSERT INTO gameplay.game_developer (developer_id, game_id, game_title)
-VALUES ('d7df9644-7803-4b70-8997-0f246665cb01','d77e1d1f-6b46-4c89-9290-3b9cf8a7c001', 'Battleship'),
-       ('d7df9644-7803-4b70-8997-0f246665cb01','d77e1d1f-6b46-4c89-9290-3b9cf8a7c002', 'Chess');
+VALUES ('2c1784ca-edf0-4882-994d-23473e30f776','d77e1d1f-6b46-4c89-9290-3b9cf8a7c001', 'Battleship'),
+       ('2c1784ca-edf0-4882-994d-23473e30f776','d77e1d1f-6b46-4c89-9290-3b9cf8a7c002', 'Chess');
 
 INSERT INTO gameplay.lobby (closed, current_player_count, max_players, game_id, id, owner_id)
 VALUES (false, 1, 2, 'd77e1d1f-6b46-4c89-9290-3b9cf8a7c002', 'ad12d056-05d1-44d7-9de6-3d896eb8bcd3', 'c7ca1ac8-f9cf-43de-a82c-fb85f84348f2'),
@@ -9,7 +9,7 @@ VALUES (false, 1, 2, 'd77e1d1f-6b46-4c89-9290-3b9cf8a7c002', 'ad12d056-05d1-44d7
        (true, 1, 2, 'd77e1d1f-6b46-4c89-9290-3b9cf8a7c002', 'a7809037-78a7-428d-ab78-16f30f41a6c6', '86da4cad-2dc1-4a43-981f-d900e8ad9dc7');
 
 INSERT INTO gameplay.game_invite (accepted, id, invited, inviter, lobby_id)
-VALUES (false, '1ff92235-07f7-4992-9851-21a32e998b4b', '9f01b00e-e627-497c-975c-452451cc0b55', 'c7ca1ac8-f9cf-43de-a82c-fb85f84348f2', 'ad12d056-05d1-44d7-9de6-3d896eb8bcd3'),
-       (false, 'ef6ed5cc-772d-4647-8d16-4cc0a666ba8d', '9f01b00e-e627-497c-975c-452451cc0b55', '86da4cad-2dc1-4a43-981f-d900e8ad9dc7', 'd802ab36-5d8b-4393-9ed2-2bd063f27c01'),
-       (true, '5a456874-ada0-42e8-90f5-0e0c9d7fc64d', '9f01b00e-e627-497c-975c-452451cc0b55', '86da4cad-2dc1-4a43-981f-d900e8ad9dc7', 'd802ab36-5d8b-4393-9ed2-2bd063f27c01'),
-       (false, 'a4a72562-35ab-4002-87f4-198a5832d0e3', '9f01b00e-e627-497c-975c-452451cc0b55', '86da4cad-2dc1-4a43-981f-d900e8ad9dc7', 'a7809037-78a7-428d-ab78-16f30f41a6c6');
+VALUES (false, '1ff92235-07f7-4992-9851-21a32e998b4b', 'cd281b11-e41e-4f12-93c8-1e9a8bf7403c', '6ca3863d-d8db-4588-b799-704fdb267814', 'ad12d056-05d1-44d7-9de6-3d896eb8bcd3'),
+       (false, 'ef6ed5cc-772d-4647-8d16-4cc0a666ba8d', 'cd281b11-e41e-4f12-93c8-1e9a8bf7403c', 'b20f4436-e61c-46f0-a83f-45f6a9cb45f9', 'd802ab36-5d8b-4393-9ed2-2bd063f27c01'),
+       (true, '5a456874-ada0-42e8-90f5-0e0c9d7fc64d', 'cd281b11-e41e-4f12-93c8-1e9a8bf7403c', 'b20f4436-e61c-46f0-a83f-45f6a9cb45f9', 'd802ab36-5d8b-4393-9ed2-2bd063f27c01'),
+       (false, 'a4a72562-35ab-4002-87f4-198a5832d0e3', 'cd281b11-e41e-4f12-93c8-1e9a8bf7403c', 'b20f4436-e61c-46f0-a83f-45f6a9cb45f9', 'a7809037-78a7-428d-ab78-16f30f41a6c6');
