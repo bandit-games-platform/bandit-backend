f54c1371dcf9d4e702fa0a63b9600ab438e37702
boldi-kdg <boldizsar.olajos@student.kdg.be>
2024-12-19 16:13:53 +0100

    Fixed invite tab to work for owner now, added missing logging for patch and create lobby

--- gameplay/src/main/java/be/kdg/int5/gameplay/adapters/out/db/lobby/LobbyJpaRepository.java
+++ gameplay/src/main/java/be/kdg/int5/gameplay/adapters/out/db/lobby/LobbyJpaRepository.java
@@ -2,7 +2,9 @@ package be.kdg.int5.gameplay.adapters.out.db.lobby;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface LobbyJpaRepository extends JpaRepository<LobbyJpaEntity, UUID> {
+    Optional<LobbyJpaEntity> findByOwnerIdAndGameId(UUID ownerId, UUID gameId);
 }
--- gameplay/src/main/java/be/kdg/int5/gameplay/port/in/query/PlayerCanInviteToLobbyQuery.java
+++ gameplay/src/main/java/be/kdg/int5/gameplay/port/in/query/PlayerCanInviteToLobbyQuery.java
@@ -1,8 +1,9 @@
 package be.kdg.int5.gameplay.port.in.query;
 
+import be.kdg.int5.gameplay.domain.GameId;
 import be.kdg.int5.gameplay.domain.LobbyId;
 import be.kdg.int5.gameplay.domain.PlayerId;
 
 public interface PlayerCanInviteToLobbyQuery {
-    boolean canPlayerCurrentlyInviteOthersToLobby(PlayerId requestingPlayer, LobbyId lobbyId);
+    LobbyId canPlayerCurrentlyInviteOthersToLobby(PlayerId requestingOwner, GameId gameId);
 }
