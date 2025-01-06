f7f67cf1e8750d3ddbf0f06aa4f823758d67f200
Inês da Costa Bastos <ines.dacostabastos@student.kdg.be>
2025-01-06 15:18:19 +0000

    trying with earlier image for gr

--- cicd/deploy-context-container.sh
+++ cicd/deploy-context-container.sh
@@ -55,7 +55,7 @@ fi
 echo "Bringing up container app"
 az containerapp up --name $CONTAINER_NAME --resource-group $RESOURCE_GROUP \
   --location northeurope --environment $ENVIRONMENT \
-  --image "$REG_USERNAME".azurecr.io/$IMAGE_NAME:"${CI_COMMIT_SHORT_SHA::-1}" \
+  --image "$REG_USERNAME".azurecr.io/$IMAGE_NAME" \
   --target-port $PORT --ingress external
 
 SECRET_ARGS=()
