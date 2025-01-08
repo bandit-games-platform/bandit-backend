95322505d41039bc49bc9bc4aa6439fabb851d1c
Inês da Costa Bastos <ines.dacostabastos@student.kdg.be>
2025-01-08 11:33:09 +0100

    without DependsOn

--- gameRegistry/src/main/java/be/kdg/int5/gameRegistry/adapters/out/publisher/GameRegistryRabbitMQTopology.java
+++ gameRegistry/src/main/java/be/kdg/int5/gameRegistry/adapters/out/publisher/GameRegistryRabbitMQTopology.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 
 @Configuration
-@DependsOn({"gameRegisteredQueue", "gameRegisteredGameplayQueue", "gameRegisteredStorefrontQueue"})
 public class GameRegistryRabbitMQTopology {
     public static final String GAME_REGISTRY_EVENTS_EXCHANGE = "game_registry_events";
     public static final String GAME_REGISTERED_QUEUE = "game_registered";
