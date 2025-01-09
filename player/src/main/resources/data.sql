24333e50148b850c6303593cbd73cc59c2ce2943
DeanTerneu <dean.terneu@student.kdg.be>
2025-01-09 14:51:08 +0100

    changing risk immage + shertening playername

--- player/src/main/resources/data.sql
+++ player/src/main/resources/data.sql
@@ -1,9 +1,9 @@
 INSERT INTO player.player (id, join_date, display_name, location, avatar)
-VALUES ('272c3f85-79db-4b6f-a20e-5d1c12e0e5b3', '2024-09-10 11:30', 'Dean', 'BELGIUM', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHfd3PPulVSp4ZbuBFNkePoUR_fLJQe474Ag&s'),
-       ('b20f4436-e61c-46f0-a83f-45f6a9cb45f9', '2024-10-09 23:55', 'Inês', 'PORTUGAL', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHfd3PPulVSp4ZbuBFNkePoUR_fLJQe474Ag&s'),
-       ('cd281b11-e41e-4f12-93c8-1e9a8bf7403c', '2024-08-08 06:35', 'Roman', 'NEW_ZEALAND', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHfd3PPulVSp4ZbuBFNkePoUR_fLJQe474Ag&s'),
-       ('6ca3863d-d8db-4588-b799-704fdb267814', '2024-08-08 06:35', 'Boldi', 'HUNGARY', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHfd3PPulVSp4ZbuBFNkePoUR_fLJQe474Ag&s'),
-       ('d7d4c8ca-5c72-4728-9925-8e313ded5edc', '2024-12-02 02:07', 'Smith', 'NIGERIA', 'https://upload.wikimedia.org/wikipedia/commons/d/dc/RP-1_.jpg'),
+VALUES ('272c3f85-79db-4b6f-a20e-5d1c12e0e5b3', '2024-09-10 11:30', 'Dean', 'BELGIUM', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQag-l2T5Whi4K_0h1Fc3wj7C0RnSkjf_nH7A&s'),
+       ('b20f4436-e61c-46f0-a83f-45f6a9cb45f9', '2024-10-09 23:55', 'Inês', 'PORTUGAL', 'https://static.vecteezy.com/system/resources/thumbnails/040/971/734/small_2x/ai-generated-a-beautiful-young-purebred-ragdoll-kitten-sits-on-an-orange-background-photo.jpg'),
+       ('cd281b11-e41e-4f12-93c8-1e9a8bf7403c', '2024-08-08 06:35', 'Roman', 'NEW_ZEALAND', 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Colosseo_2020.jpg/1200px-Colosseo_2020.jpg'),
+       ('6ca3863d-d8db-4588-b799-704fdb267814', '2024-08-08 06:35', 'Boldi', 'HUNGARY', 'https://play-lh.googleusercontent.com/EmrK8BpaVWXsbTlw92s4Np1OrJ2rxyC7I3eU3l2BQGD3IOFsBD0SrV-1YV4yGVPHli2O=w3840-h2160-rw'),
+       ('d7d4c8ca-5c72-4728-9925-8e313ded5edc', '2024-12-02 02:07', 'Smith', 'NIGERIA', 'https://i.pinimg.com/736x/9a/63/06/9a6306db28f70d27f39249e0044da128.jpg'),
        ('d7f9b8e3-3c4f-4b9f-8d7e-e1c0f4a7b901', '2024-12-02 02:07', 'BoldInYourFace', 'HUNGARY', 'https://cdn-icons-png.flaticon.com/512/3393/3393852.png'),
        ('94dad160-f5c8-4817-8f2d-611e1436ffcd', '2024-12-02 02:07', 'GeekInês418', 'PORTUGAL', 'https://cdn-icons-png.flaticon.com/512/1985/1985783.png'),
        ('8449ba7c-194c-4e51-b060-cd88cc498836', '2024-12-02 02:07', 'DebtCollector', 'NIGERIA', 'https://cdn-icons-png.flaticon.com/512/6111/6111637.png');
